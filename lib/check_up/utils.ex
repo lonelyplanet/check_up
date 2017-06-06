e603e10f103a8e8c7a33444a7c299a07663e6b54
Ben Falk <ben.falk@yahoo.com>
2017-06-06 16:00:32 -0500

    Remove warning on unused code

--- lib/check_up/utils.ex
+++ lib/check_up/utils.ex
@@ -1,11 +1,5 @@
 defmodule CheckUp.Utils do
   def url_from_conn(conn) do
-    scheme = case conn.scheme do
-      :http  -> "http"
-      :https -> "https"
-      _any   -> nil
-    end
-
     query = case conn.query_string do
       ""  -> nil
       nil -> nil
